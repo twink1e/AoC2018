29395bc9f032d9d3ed9e2a0e9d2f7bc03cf6bf54
AshleySi <junke.si@grab.com>
2018-12-16 18:25:52 +0800

    refactor

--- Day15.kts
+++ Day15.kts
@@ -2,9 +2,7 @@ import java.io.File
 import java.util.*
 import kotlin.system.exitProcess
 
-// input processing
-val inputs = File("inputs/day15.txt").readLines()
-
+// Unit class for a goblin or elf
 class Unit(row: Int, col: Int) {
     var row = row
     var col = col
@@ -16,72 +14,86 @@ class Unit(row: Int, col: Int) {
     }
 }
 
-val elfs = mutableListOf<Unit>()
-val goblins = mutableListOf<Unit>()
-val rowNum = inputs.count()
-val colNum = inputs[0].count()
+// constants
+val elf = 'E'
+val goblin = 'G'
+val space = '.'
+val hit = 3
+// following the reading order: up, left, right, down
 val rowDir = intArrayOf(-1, 0, 0, 1)
 val colDir = intArrayOf(0, -1, 1, 0)
 
+// input processing
+val inputs = File("inputs/day15.txt").readLines()
+
+val rowNum = inputs.count()
+val colNum = inputs[0].count()
 val map = Array(rowNum, { CharArray(colNum) })
-val ignore = mutableListOf<Pair<Int, Int>>()
+val elfs = mutableListOf<Unit>()
+val goblins = mutableListOf<Unit>()
+val ignored = mutableListOf<Pair<Int, Int>>()
+
 for (i in 0 until rowNum) {
     for (j in 0 until colNum) {
         val c = inputs[i][j]
         map[i][j] = c
-        if (c == 'G') {
+        if (c == goblin) {
             goblins.add(Unit(i, j))
-        } else if (c == 'E') {
+        } else if (c == elf) {
             elfs.add(Unit(i, j))
         }
     }
 }
 
+// Simulation
 var rounds = 1
-val hurt = 3
-var elfPower = 12
-val elfCount = elfs.count()
+
 while(true) {
-    ignore.clear()
-    //println("================== Round $rounds ====================")
+    ignored.clear()
+    // println("================== Round $rounds ====================")
     for (i in 0 until rowNum) {
         for (j in 0 until colNum) {
-            if (Pair(i, j) in ignore) continue
-            round(i, j, map[i][j])
+            // ignore the already moved unit in this round
+            if (Pair(i, j) in ignored) continue
+            takeAction(i, j, map[i][j])
         }
     }
     rounds++
 }
 
-fun round(row: Int, col: Int, c: Char) {
-    if (c != 'G' && c != 'E') return
-    if (c == 'G' && elfs.count() == 0 || c == 'E' && goblins.count() == 0) getResult()
+fun takeAction(row: Int, col: Int, c: Char) {
+    if (c != goblin && c != elf) return
+    if (c == goblin && elfs.count() == 0 || c == elf && goblins.count() == 0) getResult()
     if (attack(row, col, c)) return
     val newPos = move(row, col, c)
     attack(newPos.first, newPos.second, c)
 }
 
 fun move(row: Int, col: Int, c: Char): Pair<Int, Int> {
-    val targets = if (c == 'E') goblins else elfs
+    val targets = if (c == elf) goblins else elfs
     var minDist = Int.MAX_VALUE
     var distMap: Array<IntArray>? = null
+    
+    // all possible destination squares in reading order
     val squares = mutableListOf<Pair<Int, Int>>()
      for (unit in targets) {
         for (i in 0..3) {
             val currRow = unit.row+rowDir[i]
             val currCol = unit.col+colDir[i]
-            if (map[currRow][currCol] == '.') squares.add(Pair(currRow, currCol))
+            if (map[currRow][currCol] == space) squares.add(Pair(currRow, currCol))
         }
     } 
     squares.sortWith(compareBy({ it.first }, { it.second }))
-    for (t in squares) {
-        val d = reachable(row, col, t.first, t.second)
+
+    for (s in squares) {
+        val d = reachable(row, col, s.first, s.second)
         if (d[row][col] < minDist) {
             minDist = d[row][col]
             distMap = d
         }
     }
     
+    // no reachable target
     if (distMap == null) return Pair(row, col)
     
     var closetRow = 0
@@ -97,21 +109,13 @@ fun move(row: Int, col: Int, c: Char): Pair<Int, Int> {
             closetCol = currCol
         }
     }
-    val enemy = if (c == 'E') 'G' else 'E'
-    val life = removeUnit(enemy, row, col)
-    map[closetRow][closetCol] = c
-    val newUnit = Unit(closetRow, closetCol)
-    newUnit.life = life
-    if (c == 'E') {
-        elfs.add(newUnit)
-    } else {
-        goblins.add(newUnit)
-    }
-    ignore.add(Pair(closetRow, closetCol))
+    addUnit(c, closetRow, closetCol, removeUnit(c, row, col))
+    ignored.add(Pair(closetRow, closetCol))
+    // println("$row, $col $c moved to $closetRow, $closetCol")
     return Pair(closetRow, closetCol)
-    println("$row, $col $c moved to $closetRow, $closetCol")
 }
 
+// BFS
 fun reachable(fromRow: Int, fromCol: Int, toRow: Int, toCol: Int): Array<IntArray> {
     //println("reaching $toRow, $toCol from $fromRow, $fromCol")
     var q = LinkedList<Pair<Int, Int>>()
@@ -132,15 +136,13 @@ fun reachable(fromRow: Int, fromCol: Int, toRow: Int, toCol: Int): Array<IntArra
             val newRow = curRow + rowDir[i]
             val newCol = curCol + colDir[i]
             if (distMap[newRow][newCol] != Int.MAX_VALUE) continue
-            if (newRow == fromRow && newCol == fromCol || map[newRow][newCol] == '.') {
+            if (newRow == fromRow && newCol == fromCol || map[newRow][newCol] == space) {
                 distMap[newRow][newCol] = distMap[curRow][curCol] + 1
                 nextQ.add(Pair(newRow, newCol))
-                //println("$curRow, $curCol ${distMap[curRow][curCol]} add $newRow, $newCol ${map[newRow][newCol]} ${distMap[newRow][newCol]}")
                 if (newRow == fromRow && newCol == fromCol) return distMap
             }
         }
     }
- 
     return distMap
 }
 
@@ -153,7 +155,7 @@ fun attack(row: Int, col: Int, c: Char): Boolean {
         val newRow = row + rowDir[i]
         val newCol = col + colDir[i]
         val neighbor = map[newRow][newCol]
-        if ( c == 'E' && neighbor == 'G' || c == 'G' && neighbor == 'E') {
+        if (c == elf && neighbor == goblin || c == goblin && neighbor == elf) {
             attacked = true
             val target = getTarget(c, newRow, newCol)
             if (target.life < minLife) {
@@ -165,9 +167,9 @@ fun attack(row: Int, col: Int, c: Char): Boolean {
     }
     if (attacked) {
         val target = getTarget(c, minRow!!, minCol!!)
-        target.life = target.life - if (c == 'G') hurt else elfPower
+        target.life = target.life - hit
         if (target.life <= 0) {
-            removeUnit(c, minRow, minCol)
+            removeEnemyUnit(c, minRow, minCol)
         }
         //println("$row, $col $c attack $minRow, $minCol ${map[minRow][minCol]} life ${target.life}")
     } else {
@@ -176,38 +178,58 @@ fun attack(row: Int, col: Int, c: Char): Boolean {
     return attacked
 }
 
+fun addUnit(c: Char, row: Int, col: Int, life: Int) {
+    map[row][col] = c
+    val newUnit = Unit(row, col)
+    newUnit.life = life
+    if (c == goblin) {
+        goblins.add(newUnit)
+    }else {
+        elfs.add(newUnit)
+    }
+}
+
+fun removeEnemyUnit(c: Char, row: Int, col: Int): Int {
+    if (c == elf) {
+        return removeUnit(goblin, row, col)
+    } else {
+        return removeUnit(elf, row, col)
+    }
+}
+
 fun removeUnit(c: Char, row: Int, col: Int): Int {
     val mock = Unit(row, col)
-    map[row][col] = '.'
-    if (c == 'E') {
+    map[row][col] = space
+    if (c == goblin) {
         return goblins.removeAt(goblins.indexOf(mock)).life
     }else {
         return elfs.removeAt(elfs.indexOf(mock)).life
     }
 }
+
 fun getTarget(c: Char, row: Int, col: Int): Unit {
     val mock = Unit(row, col)
-    if (c == 'E') { 
+    if (c == elf) { 
         return goblins[goblins.indexOf(mock)]
     } else { 
         return elfs[elfs.indexOf(mock)]
     }
 }
 
-fun getResult(): Int {
+fun getResult() {
     var life = elfs.fold(0) { acc, unit -> acc + unit.life }
     life += goblins.fold(0) { acc, unit -> acc + unit.life }
     printMap()
+    // only count completed round
     println("${rounds - 1} $life")
     println((rounds - 1) * life)
-    println(elfCount - elfs.count())
     exitProcess(0)
 }
 
 fun printMap() {
-    for (ii in 0 until rowNum) {
-        for (jj in 0 until colNum) {
-            print(map[ii][jj])
+    for (i in 0 until rowNum) {
+        for (j in 0 until colNum) {
+            print(map[i][j])
         }
         println()
     }
